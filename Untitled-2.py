37558eeb4a1830bdf3048e1f905c03db03544d3a
Somer897 <tyme897@gmail.com>
2025-10-26 22:43:52 +0300

    123456

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,23 +1,49 @@
-a = (input()).split()
-b = 0
-choice = int(input('Введите операцию: '))
-for i in range(0, len(a)):
-    a[i] = int(a[i])
-match choice:
-    case 0:
-        for i in range(0, len(a)):
-            a[i] = a[i] * 2
-    case 1:
-        for i in range(0, len(a), 2):
-            a[i] = a[i] + 10
-    case 2:
-        for i in range(0, len(a), 3):
-            if a[i] >= 0:
-                a[i] = a[i] ** 0.5
-            else:
-                a[i] = 0
-    case _:
-     for i in range(0, len(a)):
-         b += a[i]   #b = sum(a)
-print(a)
-print(b)
+def bubble_sort(nums):
+    swapped = True
+
+    nums = list(nums)                       #чтобы работали реверсы
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+
+    n = 0                                      #параметры для сравнения
+    k = 0
+
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):                #цикл для смен
+
+            n = 0                             #обновление переменных для новой итерации
+            k =0
+
+            for i in range(len(nums) - 1):               #счетчик инверсий обычного списка
+                if nums[i] > nums[i + 1]:
+                    n +=1
+
+            if k == 0:           #чтобы в конце вернуть список к исходу (лишь для след операции)
+                nums.reverse()
+                for i in range(len(nums) - 1):                 #счетчик обратного списка
+                    if nums[i] < nums[i + 1]:
+                        k += 1
+                nums.reverse()
+            
+            if n == 0:                               #если инверсии кончились
+                swapped = False
+
+            elif n >= k:                                     #меняем местами если в ту сторону больше инверсий
+                for i in range(len(nums) - 1):
+                    if nums[i] > nums[i + 1]:
+                        nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+
+            elif k > n:                                     #меняем, если в обратную сторону больше инверсий
+                nums.reverse()
+                for i in range(len(nums) - 1):
+                    if nums[i] < nums[i + 1]:
+                        nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                nums.reverse()
+                swapped = True
+
+print('0')
+random_list_of_nums = [5, 2, 1, 8, 4]
+bubble_sort(random_list_of_nums)
+print(random_list_of_nums)
